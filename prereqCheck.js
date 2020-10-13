c8679611f4200a07b24823ea670ffed48f3e1e1b
Hunter Werenskjold <hunter@skeletoncrew.co.jp>
2020-10-13 12:03:53 +0900

    Permission Error Checking

--- prereqCheck.js
+++ prereqCheck.js
@@ -1,5 +1,34 @@
 var prereqElm = document.getElementById("prereq")
 
+// Disable Alerts
 window.alert = function ( text ) {
     return true;
   };
+
+  function checkSupportedBrowser() {
+
+    function isSupportedBrowser() {
+        var isWebRTCSupported = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia;
+
+        if (window.navigator.userAgent.indexOf("Edge") > -1) {
+            return false;
+        }
+
+        if (isWebRTCSupported) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    if(!isSupportedBrowser())
+        window.location = "https://www.google.com";
+    else
+        prereqElm.remove();
+
+  }
+
