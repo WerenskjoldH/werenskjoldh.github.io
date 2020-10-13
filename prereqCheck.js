e9a379707776b056773f087ff0ecca43a9877cc9
Hunter Werenskjold <hunter@skeletoncrew.co.jp>
2020-10-13 10:47:35 +0900

    Test Prereq

---
+++ prereqCheck.js
@@ -0,0 +1,24 @@
+var prereqElm = document.getElementById("prereq")
+
+function checkPrereqs() {
+    failedCheck = false
+
+    navigator.mediaDevices.enumerateDevices().then(devices => {
+        devices.forEach(d => {
+            if(d.label == "")
+            {
+                alert("Device unsupported");
+                failedCheck = true;
+            }
+        })
+    })
+
+    if(!failedCheck)
+    {
+        prereqElm.style.display = "none"
+    }
+    else{
+        const newElm = document.createTextNode("Error")
+        prereqElm.appendChild(newElm)
+    }
+}
